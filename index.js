18230d06951cf29678987f0d0fb8f27486e829f8
Anna0192 <nancyt33333@gmail.com>
2019-01-12 16:59:53 +0300

    files added

--- index.js
+++ index.js
@@ -1,88 +1,32 @@
 $(".user-query").on("keyup", function() {
   var inputValue = $('.user-query').val().toLowerCase();
-  $("#flights-info tr.departure, #flights-info tr.arrival ").filter(function() {
+  $("#flights-info tr").not('.table-head').filter(function() {
     $(this).toggle($(this).children('.flight-n').text().toLowerCase().indexOf(inputValue) > -1)
   });
 });
 
-
-
-const show_departuresCheckbox = document.querySelector("#displ-departures");
-const show_arrivalsCheckbox = document.querySelector('#displ-arrivals');
-const show_allCheckbox = document.querySelector('#displ-all');
-const show_delayedCheckbox = document.querySelector('#displ-delayed');
-
-
-
-show_departuresCheckbox.addEventListener('click', (event) => {
-	if (show_departuresCheckbox.checked == true) {
-		var elemsToHide = document.getElementsByClassName("arrival"); //elemsToHide is an array
-    	for(var i = 0; i < elemsToHide.length; i++){
-        
-        	elemsToHide[i].style.display = "none"; 
-    	}
-
-    	var departuresToShow = document.getElementsByClassName("departure") ;
-
-    	for(var i = 0; i < departuresToShow.length; i++){
-        
-        	departuresToShow[i].style.display = "table-row"; 
-    	}
-
-
-
-    	
-	}
-});
-
-show_arrivalsCheckbox.addEventListener('click', (event) => {
-	if (show_arrivalsCheckbox.checked == true) {
-		var elemsToHide = document.getElementsByClassName("departure"); //elemsToHide is an array
-    	for(var i = 0; i < elemsToHide.length; i++){
-        
-        	elemsToHide[i].style.display = "none"; 
-    	}
-
-    	var arrivalsToShow = document.getElementsByClassName("arrival") ; 
-		
-		for ( var i = 0; i < arrivalsToShow.length; i++ ) {        
-        	arrivalsToShow[i].style.display = "table-row"; 
-    	}
-
-	}
+$("label[for*='displ-delayed']").on("click", function() {
+  
+  $("#flights-info tr").not('.table-head').filter(function() {
+    $(this).toggle($(this).children('td:last-of-type').text().toLowerCase().indexOf('delayed') > -1)
+  });
 });
 
 
-
-
-
-show_allCheckbox.addEventListener('click', (event) => {
-	if (show_allCheckbox.checked == true) {
-		var arrivalsToShow = document.getElementsByClassName("arrival") ; 
-		
-		for(var i = 0; i < arrivalsToShow.length; i++){
-        
-        	arrivalsToShow[i].style.display = "table-row"; 
-    	}
-
-    	var departuresToShow = document.getElementsByClassName("departure") ;
-
-    	for(var i = 0; i < departuresToShow.length; i++){
-        
-        	departuresToShow[i].style.display = "table-row"; 
-    	}
-	}
+$("label[for*='displ-all']").on('click', function () {
+	$("#flights-info tr:hidden").toggle();
 });
 
-show_delayedCheckbox.addEventListener('click', (event) => {
-	if (show_delayedCheckbox.checked == true)  {
-		var elemstoHide = document.getElementsByClassName('as_planned');
-
-		for (var elem = 0; elem < elemstoHide.length; elem++ ) {
-			elemstoHide[elem].style.display = 'none';
-		}
-	}
-
+$("label[for*='displ-arrivals']").on('click', function() {
+  $("#flights-info tr").not('.table-head').filter(function() {
+    $(this).toggle($(this).find('.e-text-hidden').text().indexOf('arrival') > -1)
+  });
 });
 
 
+$("label[for*='displ-departures']").on('click', 
+function() {
+  $("#flights-info tr").not('.table-head').filter(function() {
+    $(this).toggle($(this).find('.e-text-hidden').text().toLowerCase().indexOf('departure') > -1)
+  });
+});
